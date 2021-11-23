f82e72859a9de8976d7352a7dcbdbc5a083169db
robben <>
2021-11-23 17:57:52 +0400

    添加缓存

---
+++ src/main/java/com/gameplat/admin/constant/CachedKeys.java
@@ -0,0 +1,10 @@
+package com.gameplat.admin.constant;
+
+public interface CachedKeys {
+
+  String MEMBER_LEVEL_CACHE = "member:level:";
+
+  String DICT_DATA_CACHE = "dict:data:";
+
+  String MEMBER_REMARK_CACHE = "member:remark:";
+}

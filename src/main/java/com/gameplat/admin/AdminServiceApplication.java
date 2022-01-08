536c4ca84298504ff4d2a0b1b59ae5ce4b7e20ed
robben <>
2022-01-08 18:29:07 +0400

    格式化代码

--- src/main/java/com/gameplat/admin/AdminServiceApplication.java
+++ src/main/java/com/gameplat/admin/AdminServiceApplication.java
@@ -8,8 +8,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @Slf4j
+@EnableAsync
 @EnableLogging
 @EnableCaching(basePackages = "com.gameplat")
 @EnableDiscoveryClient
@@ -18,9 +20,9 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication(scanBasePackages = "com.gameplat")
 public class AdminServiceApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(AdminServiceApplication.class, args);
-        log.info("\n\n======================== AdminServiceApplication is running ==========================\n\n");
-    }
-
+  public static void main(String[] args) {
+    SpringApplication.run(AdminServiceApplication.class, args);
+    log.info(
+        "\n\n======================== AdminServiceApplication is running ==========================\n\n");
+  }
 }

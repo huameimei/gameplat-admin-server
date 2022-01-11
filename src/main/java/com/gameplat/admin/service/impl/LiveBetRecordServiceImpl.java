14bee45792400c2ab416ebe46bba169e4b8473e3
peter <peter168168168@outlook.com>
2022-01-11 19:56:28 +0800

    真人游戏

--- src/main/java/com/gameplat/admin/service/impl/LiveBetRecordServiceImpl.java
+++ src/main/java/com/gameplat/admin/service/impl/LiveBetRecordServiceImpl.java
@@ -1,5 +1,6 @@
 package com.gameplat.admin.service.impl;
 
+import cn.hutool.core.util.NumberUtil;
 import com.alibaba.fastjson.JSONObject;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.gameplat.admin.enums.LiveGame;
@@ -8,27 +9,32 @@ import com.gameplat.admin.model.bean.ActivityStatisticItem;
 import com.gameplat.admin.model.domain.LiveBetRecord;
 import com.gameplat.admin.model.dto.LiveBetRecordQueryDTO;
 import com.gameplat.admin.model.vo.LiveBetRecordVO;
-import com.gameplat.admin.model.vo.LiveGameResultVO;
 import com.gameplat.admin.model.vo.PageDtoVO;
 import com.gameplat.admin.service.LiveBetRecordService;
+import com.gameplat.admin.service.game.GameApi;
 import com.gameplat.base.common.exception.ServiceException;
-
+import com.gameplat.common.enums.TransferTypesEnum;
+import com.gameplat.common.game.LiveGameResult;
+import com.gameplat.elasticsearch.page.PageResponse;
+import com.gameplat.elasticsearch.service.IBaseElasticsearchService;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
-
+import javax.annotation.Resource;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang.StringUtils;
+import org.apache.poi.ss.formula.functions.T;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
-
-import javax.annotation.Resource;
 
 
 @Slf4j
@@ -38,25 +44,51 @@ import javax.annotation.Resource;
 public class LiveBetRecordServiceImpl implements LiveBetRecordService {
 
     @Resource
+    private ApplicationContext applicationContext;
+
+    @Resource
     private LiveBetRecordMapper liveBetRecordMapper;
+
+    @Resource
+    private IBaseElasticsearchService elasticsearchService;
+
+    @Transactional(propagation = Propagation.NOT_SUPPORTED)
+    public GameApi getGameApi(String liveCode) {
+        GameApi api = applicationContext.getBean(liveCode + GameApi.Suffix, GameApi.class);
+        TransferTypesEnum tt = TransferTypesEnum.get(liveCode);
+        // 1代表是否额度转换
+        if (tt == null || tt.getType() != 1) {
+            throw new ServiceException("游戏未接入");
+        }
+        return api;
+    }
 
     @Override
     public PageDtoVO<LiveBetRecordVO> queryPageBetRecord(Page<LiveBetRecordVO> page, LiveBetRecordQueryDTO dto) {
         //TODO 调用ES服务
         PageDtoVO<LiveBetRecordVO> pageDtoVO = new PageDtoVO();
+        Page resultPage = new Page();
+        //创建一个要搜索的索引库
+        SearchSourceBuilder builder = new SearchSourceBuilder();
+        String[] includes = new String[]{"billNo","account","gameType","betTime","betAmount","validAmount",
+            "winAmount","gameKind","settle","gmtBetTime","statTime","createTime","betContent"};
+        builder.fetchSource(includes,new String[]{});
+        Integer pageNum = NumberUtil.parseInt(String.valueOf(page.getCurrent()));
+        Integer pageSize = NumberUtil.parseInt(String.valueOf(page.getSize()));
+        PageResponse result = elasticsearchService.search(dto.getLiveCode(),builder,LiveBetRecord.class,pageNum,pageSize);
+        resultPage.setRecords(result.getList());
         Map<String, Object> otherData = new HashMap<>();
         otherData.put("totalData", null);
-        pageDtoVO.setPage(null);
+        pageDtoVO.setPage(resultPage);
         pageDtoVO.setOtherData(otherData);
         return pageDtoVO;
     }
 
     @Override
-    public LiveGameResultVO getGameResult(String liveCode, String billNo) {
+    public LiveGameResult getGameResult(String liveCode, String billNo) throws Exception {
         // TODO 直接连游戏查询结果
-        //GameApi gameApi = getGameApi(liveCode);
-        // gameApi.getGameResult(billNo);
-        LiveGameResultVO liveGameResult = null;
+        GameApi gameApi = getGameApi(liveCode);
+        LiveGameResult liveGameResult = gameApi.getGameResult(billNo);;
         if (StringUtils.isBlank(liveGameResult.getData())) {
             throw new ServiceException(LiveGame.getName(liveCode) + "暂不支持查看游戏结果");
         }

53dfcbf2a75de90e98f9ddf05ba4c3051d4a0b37
lily <@@@sq125>
2022-01-16 18:22:16 +0400

    修改现金流水导出列表

--- src/main/java/com/gameplat/admin/service/impl/MemberBillServiceImpl.java
+++ src/main/java/com/gameplat/admin/service/impl/MemberBillServiceImpl.java
@@ -1,7 +1,5 @@
 package com.gameplat.admin.service.impl;
 
-import cn.afterturn.easypoi.excel.ExcelExportUtil;
-import cn.afterturn.easypoi.excel.entity.ExportParams;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.core.toolkit.ObjectUtils;
@@ -17,20 +15,15 @@ import com.gameplat.admin.model.vo.MemberBillVO;
 import com.gameplat.admin.service.MemberBillService;
 import com.gameplat.admin.service.MemberService;
 import com.gameplat.base.common.exception.ServiceException;
-import java.io.IOException;
-import java.io.OutputStream;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
-import javax.servlet.http.HttpServletResponse;
 
 import com.gameplat.common.enums.TranTypes;
 import com.gameplat.common.model.bean.TranTypeBean;
-import lombok.Cleanup;
-import org.apache.poi.ss.usermodel.Workbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -60,60 +53,8 @@ public class MemberBillServiceImpl extends ServiceImpl<MemberBillMapper, MemberB
     return allTranList;
   }
 
-//  @Override
-//  public IPage<MemberBillVO> findMemberBilllist(PageDTO<MemberBill> page, MemberBillDTO dto) {
-//
-//    if (ObjectUtils.isEmpty(dto.getBeginTime()) || ObjectUtils.isEmpty(dto.getEndTime())){
-//      throw new ServiceException("查询账变记录需选择时间段！");
-//    }
-//    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-//    Date nowDate = null;
-//    Date beginTime = null;
-//    Date endTime = null;
-//    try {
-//      nowDate = sdf.parse(sdf.format(new Date()));
-//      beginTime = sdf.parse(dto.getBeginTime());
-//      endTime = sdf.parse(dto.getEndTime());
-//    } catch (ParseException e) {
-//      e.printStackTrace();
-//    }
-//
-//    //今天
-//    if (beginTime.compareTo(nowDate) == 0 && endTime.compareTo(nowDate) == 0){
-//      dto.setBeginTime(dto.getBeginTime()+" 00:00:00");
-//      dto.setEndTime(dto.getEndTime()+" 23:59:59");
-//      return this.lambdaQuery()
-//              .eq(ObjectUtils.isNotEmpty(dto.getAccount()), MemberBill::getAccount, dto.getAccount())
-//              .eq(ObjectUtils.isNotEmpty(dto.getOrderNo()), MemberBill::getOrderNo, dto.getOrderNo())
-//              .ge(ObjectUtils.isNotEmpty(dto.getBeginTime()), MemberBill::getCreateTime, dto.getBeginTime())
-//              .le(ObjectUtils.isNotEmpty(dto.getEndTime()), MemberBill::getCreateTime, dto.getEndTime())
-//              .in(ObjectUtils.isNotEmpty(dto.getTranTypes()), MemberBill::getTranType, dto.getTranTypes())
-//              .orderByDesc(MemberBill::getCreateTime)
-//              .orderByDesc(MemberBill::getId)
-//              .page(page)
-//              .convert(memberBillConvert::toVo);
-//      //历史
-//    }else {
-//      if(ObjectUtils.isEmpty(dto.getAccount())){
-//        throw new ServiceException("查询【非当日】账变记录需提供【会员帐号】！");
-//      }
-//      Integer tableIndex = this.lambdaQuery()
-//              .eq(ObjectUtils.isNotEmpty(dto.getAccount()), MemberBill::getAccount, dto.getAccount())
-//              .list().get(0).getTableIndex();
-//
-//      IPage<MemberBillVO> pageList = memberBillMapper.findyByTableIndex(page, dto.getAccount(),
-//              dto.getOrderNo(),
-//              dto.getTranTypes(),
-//              dto.getBeginTime(),
-//              dto.getEndTime(),
-//              tableIndex);
-//
-//      return pageList;
-//    }
-//  }
-
   @Override
-  public IPage<MemberBillVO> findMemberBilllist(PageDTO<MemberBill> page, MemberBillDTO dto) {
+  public IPage<MemberBillVO> findMemberBilllistPage(PageDTO<MemberBill> page, MemberBillDTO dto) {
 
     if (ObjectUtils.isEmpty(dto.getBeginTime()) || ObjectUtils.isEmpty(dto.getEndTime())){
       throw new ServiceException("查询账变记录需选择时间段！");
@@ -151,19 +92,41 @@ public class MemberBillServiceImpl extends ServiceImpl<MemberBillMapper, MemberB
   }
 
   @Override
-  public void exportList(MemberBillDTO dto, HttpServletResponse response){
-    try{
-      List<MemberBillVO> list = getList(dto);
-      response.setContentType("application/vnd.ms-excel");
-      response.setHeader("Content-disposition", "attachment;filename=myExcel.xls");
-      @Cleanup OutputStream ouputStream = null;
-      Workbook workbook = ExcelExportUtil.exportExcel(new ExportParams("VIP会员签到记录导出","VIP会员签到记录"),
-              MemberBillVO.class, list );
-      ouputStream = response.getOutputStream();
-      workbook.write(ouputStream);
-    }catch (IOException e) {
-      throw new ServiceException("导出失败:"+e);
+  public List<MemberBillVO> findMemberBillList(MemberBillDTO dto) {
+
+    if (ObjectUtils.isEmpty(dto.getBeginTime()) || ObjectUtils.isEmpty(dto.getEndTime())){
+      throw new ServiceException("查询账变记录需选择时间段！");
     }
+    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+    Date nowDate = null;
+    Date beginTime = null;
+    Date endTime = null;
+    try {
+      nowDate = sdf.parse(sdf.format(new Date()));
+      beginTime = sdf.parse(dto.getBeginTime());
+      endTime = sdf.parse(dto.getEndTime());
+    } catch (ParseException e) {
+      e.printStackTrace();
+    }
+
+    //今天
+    if (beginTime.compareTo(nowDate) == 0 && endTime.compareTo(nowDate) == 0){
+      dto.setBeginTime(dto.getBeginTime()+" 00:00:00");
+      dto.setEndTime(dto.getEndTime()+" 23:59:59");
+    }else if(ObjectUtils.isEmpty(dto.getAccount())){
+      throw new ServiceException("查询【非当日】账变记录需提供【会员帐号】！");
+    }
+    QueryWrapper<Member> memberQuery = Wrappers.query();
+    memberQuery.eq(true, "account", dto.getAccount());
+    Integer tableIndex = Optional.ofNullable(memberService.getOne(memberQuery)).map(m -> m.getTableIndex()).orElse(null);
+
+    List<MemberBillVO> pageList = memberBillMapper.findyByTableIndex(dto.getAccount(),
+            dto.getOrderNo(),
+            dto.getTranTypes(),
+            dto.getBeginTime(),
+            dto.getEndTime(),
+            tableIndex);
+    return pageList;
   }
 
   @Override

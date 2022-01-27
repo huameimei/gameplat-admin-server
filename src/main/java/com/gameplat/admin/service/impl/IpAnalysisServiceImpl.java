7465ef3e051d3d7363377f2e5d932516686fb124
lily <@@@sq125>
2022-01-27 15:17:06 +0400

    修改

--- src/main/java/com/gameplat/admin/service/impl/IpAnalysisServiceImpl.java
+++ src/main/java/com/gameplat/admin/service/impl/IpAnalysisServiceImpl.java
@@ -1,24 +1,51 @@
 package com.gameplat.admin.service.impl;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.core.toolkit.CollectionUtils;
+import com.baomidou.mybatisplus.core.toolkit.ObjectUtils;
 import com.baomidou.mybatisplus.extension.plugins.pagination.PageDTO;
 import com.gameplat.admin.enums.IpAnalysisEnum;
 import com.gameplat.admin.mapper.MemberMapper;
 import com.gameplat.admin.mapper.MemberWithdrawHistoryMapper;
 import com.gameplat.admin.mapper.RechargeOrderHistoryMapper;
 import com.gameplat.admin.model.dto.IpAnalysisDTO;
+import com.gameplat.admin.model.vo.IpAnalysisEsVO;
 import com.gameplat.admin.model.vo.IpAnalysisVO;
 import com.gameplat.admin.service.IpAnalysisService;
 import com.gameplat.base.common.exception.ServiceException;
+import com.gameplat.base.common.util.StringUtils;
+import com.gameplat.elasticsearch.page.PageResponse;
+import com.gameplat.elasticsearch.service.IBaseElasticsearchService;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.index.query.BoolQueryBuilder;
+import org.elasticsearch.index.query.QueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.rest.RestStatus;
+import org.elasticsearch.search.aggregations.*;
+import org.elasticsearch.search.aggregations.bucket.terms.Terms;
+import org.elasticsearch.search.aggregations.bucket.terms.TermsAggregationBuilder;
+import org.elasticsearch.search.aggregations.metrics.CardinalityAggregationBuilder;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
+import org.elasticsearch.search.sort.FieldSortBuilder;
+import org.elasticsearch.search.sort.SortBuilder;
+import org.elasticsearch.search.sort.SortBuilders;
+import org.elasticsearch.search.sort.SortOrder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 
 /**
@@ -40,9 +67,20 @@ public class IpAnalysisServiceImpl implements IpAnalysisService {
     private MemberMapper memberMapper;
 
     @Autowired
+    private IBaseElasticsearchService baseElasticsearchService;
+
+    @Autowired
     private RedisTemplate<String, Object> redisTemplate;
 
+    @Autowired
+    private RestHighLevelClient restHighLevelClient;
+
     private static final String TOKEN_PREFIX = "token:web:";
+
+    @Value("${syslog.tenant}")
+    private String tenant;
+
+    private static final String INDEX = "login-log_";
 
     /** ip分析 */
     @Override
@@ -58,7 +96,38 @@ public class IpAnalysisServiceImpl implements IpAnalysisService {
             pagelist = memberMapper.page(page, dto);
             list = pagelist.getRecords();
         } else if (IpAnalysisEnum.LOGIN.getCode().equals(dto.getType())) {
-            // 登录
+        // 登录
+            QueryBuilder builder = IpAnalysisServiceImpl.buildSearch(dto);
+            //按时间排序
+            SortBuilder<FieldSortBuilder> sortBuilder = SortBuilders.fieldSort("createTime.keyword").order(SortOrder.DESC);
+            PageResponse<JSON> result = baseElasticsearchService.search(builder, INDEX + tenant, JSON.class,
+                (int) page.getCurrent(), (int) page.getSize(), sortBuilder);
+            List<JSON> log = result.getList();
+            for (int i = 0; i < log.size(); i++) {
+                IpAnalysisEsVO esVO = JSON.toJavaObject(log.get(i), IpAnalysisEsVO.class);
+                IpAnalysisVO vo = new IpAnalysisVO(){{
+                    setAccount(esVO.getUsername());
+                    setIpAddress(esVO.getIpAddress());
+                }};
+                list.add(vo);
+                pagelist.setRecords(list);
+
+        }
+            pagelist.setTotal(list.size());
+            if (ObjectUtils.isNull(page.getSize())){
+                pagelist.setSize(10);
+            }else{
+                pagelist.setSize(page.getSize());
+            }
+            if (ObjectUtils.isNull(page.getCurrent())){
+                pagelist.setCurrent(1);
+            }else{
+                pagelist.setCurrent(page.getCurrent());
+            }
+            pagelist.setPages(page.getTotal() % page.getSize() == 0 ? page.getTotal() / page.getSize() : page.getTotal() / page.getSize() + 1);
+
+        searchEs(dto);
+        aggregationSearchDoc(dto);
         } else if (IpAnalysisEnum.RECHARGE.getCode().equals(dto.getType())) {
             // 充值
             pagelist = rechargeOrderHistoryMapper.page(page, dto);
@@ -81,5 +150,80 @@ public class IpAnalysisServiceImpl implements IpAnalysisService {
             }
         }
         return pagelist;
+    }
+
+    public static QueryBuilder buildSearch(IpAnalysisDTO dto) {
+        BoolQueryBuilder builder = QueryBuilders.boolQuery();
+        if (StringUtils.isNotBlank(dto.getAccount())) {
+            builder.must(QueryBuilders.termQuery("username", dto.getAccount()));
+        }
+        if (ObjectUtils.isNotNull(dto.getMemberId())) {
+            builder.must(QueryBuilders.matchQuery("memberId", dto.getMemberId()));
+        }
+        if (ObjectUtils.isNotNull(dto.getLoginIp())) {
+            builder.must(QueryBuilders.matchQuery("ipAddress", dto.getLoginIp()));
+        }
+        if (ObjectUtils.isNotNull(dto.getBeginTime()) && ObjectUtils.isNotNull(dto.getEndTime())) {
+            builder.must(QueryBuilders.rangeQuery("createTime").from(dto.getBeginTime()).to(dto.getEndTime()));
+        }
+//        builder.must(QueryBuilders.matchQuery("subject", "web"));
+        return builder;
+    }
+
+    public void searchEs(IpAnalysisDTO dto){
+        // 创建search请求
+        SearchRequest searchRequest = new SearchRequest(INDEX+tenant);
+        // 用SearchSourceBuilder来构造查询请求体
+        SearchSourceBuilder sourceBuilder = new SearchSourceBuilder().query(IpAnalysisServiceImpl.buildSearch(dto));
+        // 去重
+        CardinalityAggregationBuilder distinct_count = AggregationBuilders.cardinality("distinct_count")
+                .field("username.keyword");
+        // 根据 会员账号 ip 分组 而后根据 会员账号去重 而后根据时间排序
+        TermsAggregationBuilder aggregation = AggregationBuilders.terms("by_group")
+                .field("username.keyword")
+                .field("ipAddress.keyword")
+                .subAggregation(distinct_count)
+                .order(BucketOrder.aggregation("distinct_count", false));
+
+        sourceBuilder.aggregation(aggregation);
+        searchRequest.source(sourceBuilder);
+        //3、发送请求
+        SearchResponse searchResponse = null;
+        try {
+            searchResponse = restHighLevelClient.search(searchRequest, RequestOptions.DEFAULT);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if (RestStatus.OK.equals(searchResponse.status())) {
+            // 获取聚合结果
+            Aggregations aggregations = searchResponse.getAggregations();
+            Terms byAgeAggregation = aggregations.get("by_group");
+            for (Terms.Bucket buck : byAgeAggregation.getBuckets()) {
+                String ip = buck.getKeyAsString();
+                Aggregations aggregations1 = buck.getAggregations();
+                Aggregation distinct_count1 = aggregations1.get("distinct_count");
+                JSONObject jsonObject = JSON.parseObject(JSON.toJSONString(distinct_count1));
+                System.out.println(jsonObject);
+                String cardinalityValue = jsonObject.getString("username");
+                System.out.println(ip +"\t\t" + cardinalityValue);
+            }
+        }
+    }
+
+    //聚合查詢
+    public Map<String, Long> aggregationSearchDoc(IpAnalysisDTO dto){
+        String index = INDEX+tenant;
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder().query(IpAnalysisServiceImpl.buildSearch(dto));
+        CardinalityAggregationBuilder distinct_count = AggregationBuilders.cardinality("distinct_count").field("username.keyword");
+
+        TermsAggregationBuilder aggregation = AggregationBuilders.terms("by_group")
+                .field("username.keyword")
+                .field("ipAddress.keyword")
+                .subAggregation(distinct_count)
+                .order(BucketOrder.aggregation("distinct_count", false));
+        searchSourceBuilder.aggregation(aggregation);
+        Map<String, Long> by_group = baseElasticsearchService.aggregationSearchDoc(index, searchSourceBuilder, "by_group");
+        System.out.println(by_group);
+        return by_group;
     }
 }

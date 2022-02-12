d111491c47afd357f25be1b729c032167d958010
lily <@@@sq125>
2022-02-12 12:46:13 +0400

    删除无用文件

--- src/main/java/com/gameplat/admin/mapper/SysInformationMapper.java
+++
@@ -1,7 +0,0 @@
-package com.gameplat.admin.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.gameplat.admin.model.domain.SysInformation;
-
-public interface SysInformationMapper extends BaseMapper<SysInformation> {
-}

c7288bd5d37b16394e8dd4f3e4a8ce0d0e2aab8d
lily <@@@sq125>
2022-02-13 17:53:55 +0400

    修改聊天室gif图

---
+++ src/main/java/com/gameplat/admin/model/domain/ChatGif.java
@@ -0,0 +1,54 @@
+package com.gameplat.admin.model.domain;
+
+import com.baomidou.mybatisplus.annotation.*;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * @author lily
+ * @description git
+ * @date 2022/2/10
+ */
+@Data
+@TableName("chat_gif")
+public class ChatGif implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @ApiModelProperty(value = "主键")
+    @TableId(value = "id", type = IdType.AUTO)
+    private Integer id;
+
+    @ApiModelProperty(value = "关键词")
+    private String name;
+
+    private String storeFileName;
+
+    @ApiModelProperty(value = "图片地址")
+    private String fileUrl;
+
+    private Integer height;
+
+    private Integer width;
+
+    @ApiModelProperty(value = "使用次数")
+    private Integer userCount;
+
+    private String md5;
+
+    private String serviceFileName;
+
+    @ApiModelProperty(value = "服务提供商（1.本地 2.minio 3.七牛云 4.阿里云 5.腾讯云）")
+    private Integer serviceProvider;
+
+    private String fileType;
+
+    @TableField(fill = FieldFill.INSERT)
+    private String createBy;
+
+    @TableField(fill = FieldFill.INSERT)
+    private Date createTime;
+}

3696068854fabec895438559c7d6598fd45b53c3
cc <544765885@qq.com>
2022-02-21 00:56:56 +0800

    代理配置限制信息

---
+++ src/main/java/com/gameplat/admin/model/vo/FissionDivideLevelVo.java
@@ -0,0 +1,16 @@
+package com.gameplat.admin.model.vo;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+/**
+ * @Description : 裂变等级对应分红率
+ * @Author : cc
+ * @Date : 2022/2/4
+ */
+@Data
+public class FissionDivideLevelVo {
+    private Integer level;
+    private BigDecimal levelDivideRatio;
+}

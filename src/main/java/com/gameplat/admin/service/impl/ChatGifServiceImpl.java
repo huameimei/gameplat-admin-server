e58d6a8ed79c180808d99d2132c2de623536670e
lily <@@@sq125>
2022-02-23 21:53:42 +0400

    修改git

--- src/main/java/com/gameplat/admin/service/impl/ChatGifServiceImpl.java
+++ src/main/java/com/gameplat/admin/service/impl/ChatGifServiceImpl.java
@@ -40,127 +40,127 @@ import java.util.Map;
  * @description 聊天室Gif管理
  * @date 2022/2/13
  */
-
 @Service
 @Slf4j
 @RequiredArgsConstructor
 @Transactional(isolation = Isolation.DEFAULT, rollbackFor = Throwable.class)
-public class ChatGifServiceImpl extends ServiceImpl<ChatGifMapper, ChatGif> implements ChatGifService {
+public class ChatGifServiceImpl extends ServiceImpl<ChatGifMapper, ChatGif>
+    implements ChatGifService {
 
-    @Autowired
-    private ChatGifConvert chatGifConvert;
+  @Autowired private ChatGifConvert chatGifConvert;
 
-    @Autowired
-    private ConfigService configService;
+  @Autowired private ConfigService configService;
 
-    @Autowired
-    private FileStorageStrategyContext fileStorageStrategyContext;
+  @Autowired private FileStorageStrategyContext fileStorageStrategyContext;
 
-    /** 分页列表 */
-    @Override
-    public IPage<ChatGifVO> page(PageDTO<ChatGif> page, String name) {
-        IPage<ChatGifVO> pageList = this.lambdaQuery()
-                .like(StringUtils.isNotNull(name), ChatGif::getName, name)
-                .orderByDesc(ChatGif::getCreateTime)
-                .page(page)
-                .convert(chatGifConvert::toVo);
-//        FileConfig fileConfig = configService.getDefaultConfig(DictTypeEnum.FILE_CONFIG, FileConfig.class);
-//        for (int i = 0; i < pageList.getRecords().size(); i++) {
-//            if (!pageList.getRecords().get(i).getFileUrl().startsWith("http") || !pageList.getRecords().get(i).getFileUrl().startsWith("https")) {
-//                pageList.getRecords().get(i).setFileUrl(fileConfig.getEndpoint()+"/" + pageList.getRecords().get(i).getFileUrl());
-//            }
-//        }
-        return pageList;
+  /** 分页列表 */
+  @Override
+  public IPage<ChatGifVO> page(PageDTO<ChatGif> page, String name) {
+    IPage<ChatGifVO> pageList =
+        this.lambdaQuery()
+            .like(StringUtils.isNotNull(name), ChatGif::getName, name)
+            .orderByDesc(ChatGif::getCreateTime)
+            .page(page)
+            .convert(chatGifConvert::toVo);
+    return pageList;
+  }
+
+  /** 增 */
+  @Override
+  public void add(MultipartFile file, String name) throws Exception {
+    String url = upload(file);
+    File file1 = new File(file.getOriginalFilename());
+    try {
+      FileUtils.copyInputStreamToFile(file.getInputStream(), file1);
+    } catch (IOException e) {
+      log.info("异常原因{}", e);
     }
-
-    /** 增 */
-    @Override
-    public void add(MultipartFile file, String name) throws Exception {
-        String url = upload(file);
-        File file1 = new File(file.getOriginalFilename());
-        try {
-            FileUtils.copyInputStreamToFile(file.getInputStream(), file1);
-        } catch (IOException e) {
-            log.info("异常原因{}", e);
-        }
-        // 会在本地产生临时文件，用完后需要删除
-        if (file1.exists()) {
-            file1.delete();
-        }
-        ChatGif chatGif = new ChatGif();
-        BufferedImage bufferedImage = null;
-        try {
-            // 通过MultipartFile得到InputStream，从而得到BufferedImage
-            bufferedImage = ImageIO.read(file.getInputStream());
-            if (bufferedImage!=null){
-                chatGif.setHeight(bufferedImage.getHeight());
-                chatGif.setWidth(bufferedImage.getWidth());
-            }
-        } catch (IOException e) {
-            log.info("异常原因{}", e);
-            throw new ServiceException("此图片已破损");
-        }
-//        // 特殊处理本地服务器
-//        if(FileStorageEnum.LOCAL.getValue() == fileConfig.getProvider()){
-//            //服务器名称
-//            chatGif.setServiceFileName(DyDataSourceContextHolder.getTenant() + "/" + fType + fKey);
-//        }else{
-//            chatGif.setServiceFileName(fType + fKey);
-//        }
-        chatGif.setName(name);
-        if (bufferedImage!=null){
-            chatGif.setHeight(bufferedImage.getHeight());
-            chatGif.setWidth(bufferedImage.getWidth());
-        }
-        chatGif.setFileUrl(url);
-        chatGif.setMd5(SecureUtil.md5(file.getInputStream()));
-
-        //如果是异步上传，如果响应时间太长，request关闭，也就获取不到请求头的token，也就拿不到LoginAppUser对象
-        //所以需要在异步线程将上传人加入到本地线程，在保存上传记录时，上传者从本地线程中拿值
-        String userName = JSONObjectContext.getJSONObjectContext("userName");
-        if(StringUtils.isNotEmpty(userName)){
-            chatGif.setCreateBy(userName);
-        }
-        save(chatGif);
+    // 会在本地产生临时文件，用完后需要删除
+    if (file1.exists()) {
+      file1.delete();
     }
-
-    @Override
-    public String upload(MultipartFile file) throws Exception {
-        //上传图片
-        FileConfig fileConfig = configService.getDefaultConfig(DictTypeEnum.FILE_CONFIG, FileConfig.class);
-        FileStorageProvider fileStorageProvider = fileStorageStrategyContext.getProvider(fileConfig);
-        //上传成功，返回图片地址
-        String url = fileStorageProvider.upload(file.getInputStream(),file.getContentType(), file.getOriginalFilename());
-        return url;
+    ChatGif chatGif = new ChatGif();
+    BufferedImage bufferedImage = null;
+    try {
+      // 通过MultipartFile得到InputStream，从而得到BufferedImage
+      bufferedImage = ImageIO.read(file.getInputStream());
+      if (bufferedImage != null) {
+        chatGif.setHeight(bufferedImage.getHeight());
+        chatGif.setWidth(bufferedImage.getWidth());
+      }
+    } catch (IOException e) {
+      log.info("异常原因{}", e);
+      throw new ServiceException("此图片已破损");
     }
-
-    /** 删 */
-    @Override
-    public void remove(Integer id) {
-        ChatGif chatGif = this.getById(id);
-        if(ObjectUtil.isEmpty(chatGif)){
-            throw new ServiceException("存储位置未知");
-        }
-        //删储服务器图片
-        FileConfig fileConfig = configService.getDefaultConfig(DictTypeEnum.FILE_CONFIG, FileConfig.class);
-        FileStorageProvider fileStorageProvider = fileStorageStrategyContext.getProvider(fileConfig);
-        fileStorageProvider.delete(chatGif.getFileUrl());
-        //删库
-        super.removeById(id);
+    //        // 特殊处理本地服务器
+    //        if(FileStorageEnum.LOCAL.getValue() == fileConfig.getProvider()){
+    //            //服务器名称
+    //            chatGif.setServiceFileName(DyDataSourceContextHolder.getTenant() + "/" + fType +
+    // fKey);
+    //        }else{
+    //            chatGif.setServiceFileName(fType + fKey);
+    //        }
+    chatGif.setName(name);
+    if (bufferedImage != null) {
+      chatGif.setHeight(bufferedImage.getHeight());
+      chatGif.setWidth(bufferedImage.getWidth());
     }
+    chatGif.setFileUrl(url);
+    chatGif.setMd5(SecureUtil.md5(file.getInputStream()));
 
-    /** 改 */
-    @Override
-    public void edit(ChatGifEditDTO dto) {
-        this.updateById(chatGifConvert.toEntity(dto));
+    // 如果是异步上传，如果响应时间太长，request关闭，也就获取不到请求头的token，也就拿不到LoginAppUser对象
+    // 所以需要在异步线程将上传人加入到本地线程，在保存上传记录时，上传者从本地线程中拿值
+    String userName = JSONObjectContext.getJSONObjectContext("userName");
+    if (StringUtils.isNotEmpty(userName)) {
+      chatGif.setCreateBy(userName);
     }
+    save(chatGif);
+  }
 
-    @Override
-    public int findMD5(String md5) {
-        QueryWrapper<ChatGif> queryWrapper = new QueryWrapper<>();
-        queryWrapper.select( "count(*) as count").eq("md5", md5);
-        Map<String, Object> map = this.getMap(queryWrapper);
-        return Integer.parseInt(map.get("count").toString());
+  @Override
+  public String upload(MultipartFile file) throws Exception {
+    // 上传图片
+    FileConfig fileConfig =
+        configService.getDefaultConfig(DictTypeEnum.FILE_CONFIG, FileConfig.class);
+    FileStorageProvider fileStorageProvider = fileStorageStrategyContext.getProvider(fileConfig);
+    String md5 = SecureUtil.md5(file.getInputStream());
+    if (findMD5(md5) > 0) {
+      throw new ServiceException("不允许相同图片");
     }
+    // 上传成功，返回图片地址
+    String url =
+        fileStorageProvider.upload(
+            file.getInputStream(), file.getContentType(), file.getOriginalFilename());
+    return url;
+  }
 
+  /** 删 */
+  @Override
+  public void remove(Integer id) {
+    ChatGif chatGif = this.getById(id);
+    if (ObjectUtil.isEmpty(chatGif)) {
+      throw new ServiceException("存储位置未知");
+    }
+    // 删储服务器图片
+    FileConfig fileConfig =
+        configService.getDefaultConfig(DictTypeEnum.FILE_CONFIG, FileConfig.class);
+    FileStorageProvider fileStorageProvider = fileStorageStrategyContext.getProvider(fileConfig);
+    fileStorageProvider.delete(chatGif.getFileUrl());
+    // 删库
+    super.removeById(id);
+  }
+
+  /** 改 */
+  @Override
+  public void edit(ChatGifEditDTO dto) {
+    this.updateById(chatGifConvert.toEntity(dto));
+  }
+
+  @Override
+  public int findMD5(String md5) {
+    QueryWrapper<ChatGif> queryWrapper = new QueryWrapper<>();
+    queryWrapper.select("count(*) as count").eq("md5", md5);
+    Map<String, Object> map = this.getMap(queryWrapper);
+    return Integer.parseInt(map.get("count").toString());
+  }
 }

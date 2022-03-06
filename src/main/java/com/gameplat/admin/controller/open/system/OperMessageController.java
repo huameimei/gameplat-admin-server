469021dcf47b858df51697928753ab21401f13a4
gray <xiaoguidename@gmail.com>
2022-03-06 19:33:40 +0800

    系统消息

--- src/main/java/com/gameplat/admin/controller/open/system/OperMessageController.java
+++ src/main/java/com/gameplat/admin/controller/open/system/OperMessageController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 /**
  * 系统消息
  *
@@ -24,8 +26,26 @@ public class OperMessageController {
     @Autowired
     private SysMessageService sysMessageService;
 
+    /**
+     * 分页列表
+     *
+     * @param page
+     * @param dto
+     * @return
+     */
     @GetMapping("/list")
     public IPage<SysMessage> list(PageDTO<SysMessage> page, SysMessageDTO dto) {
         return sysMessageService.pageList(page, dto);
+    }
+
+    /**
+     * 最新系统消息
+     *
+     * @param dto
+     * @return
+     */
+    @GetMapping("/lastList")
+    public List<SysMessage> lastList(SysMessageDTO dto) {
+        return sysMessageService.lastList(dto);
     }
 }
--- src/main/java/com/gameplat/admin/service/SysMessageService.java
+++ src/main/java/com/gameplat/admin/service/SysMessageService.java
@@ -5,6 +5,8 @@ import com.baomidou.mybatisplus.extension.service.IService;
 import com.gameplat.admin.model.dto.SysMessageDTO;
 import com.gameplat.model.entity.sys.SysMessage;
 
+import java.util.List;
+
 /**
  * 系统消息 业务层
  *
@@ -16,4 +18,12 @@ public interface SysMessageService extends IService<SysMessage> {
      * page list
      */
     IPage<SysMessage> pageList(IPage<SysMessage> page, SysMessageDTO dto);
+
+    /**
+     * 最近的系统消息
+     *
+     * @param dto
+     * @return
+     */
+    List<SysMessage> lastList(SysMessageDTO dto);
 }

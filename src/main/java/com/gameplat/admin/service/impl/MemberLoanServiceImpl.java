6dc5cbe24c66053967af0d44608f5112da61657a
lily <@@@sq125>
2022-03-06 18:58:20 +0400

    添加借呗相关内容

--- src/main/java/com/gameplat/admin/service/impl/MemberLoanServiceImpl.java
+++ src/main/java/com/gameplat/admin/service/impl/MemberLoanServiceImpl.java
@@ -2,6 +2,7 @@ package com.gameplat.admin.service.impl;
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.gameplat.admin.mapper.MemberLoanMapper;
+import com.gameplat.admin.service.MemberLoanService;
 import com.gameplat.model.entity.member.MemberLoan;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -16,5 +17,5 @@ import org.springframework.transaction.annotation.Transactional;
 @Slf4j
 @Service
 @Transactional(isolation = Isolation.DEFAULT, rollbackFor = Throwable.class)
-public class MemberLoanServiceImpl extends ServiceImpl<MemberLoanMapper, MemberLoan> {
+public class MemberLoanServiceImpl extends ServiceImpl<MemberLoanMapper, MemberLoan> implements MemberLoanService {
 }

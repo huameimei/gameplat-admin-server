c9bd3d881d0bde27c52cf4a3a28874e3ecc5b742
lily <@@@sq125>
2022-03-07 18:36:32 +0400

    借呗欠款回收

--- src/main/java/com/gameplat/admin/service/MemberLoanService.java
+++ src/main/java/com/gameplat/admin/service/MemberLoanService.java
@@ -7,10 +7,16 @@ import com.gameplat.admin.model.dto.MemberLoanQueryDTO;
 import com.gameplat.admin.model.vo.MemberLoanVO;
 import com.gameplat.model.entity.member.MemberLoan;
 
+import java.math.BigDecimal;
+
 public interface MemberLoanService extends IService<MemberLoan> {
 
     /** 分页查 */
     IPage<MemberLoanVO> page(PageDTO<MemberLoan> page, MemberLoanQueryDTO dto);
 
     void editOrUpdate(MemberLoan memberLoan);
+
+    /** 回收 */
+    void recycle(String account);
+
 }
--- src/main/java/com/gameplat/admin/service/impl/MemberLoanServiceImpl.java
+++ src/main/java/com/gameplat/admin/service/impl/MemberLoanServiceImpl.java
@@ -1,5 +1,6 @@
 package com.gameplat.admin.service.impl;
 
+import cn.hutool.core.date.DateUtil;
 import cn.hutool.core.util.ObjectUtil;
 import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
@@ -11,15 +12,13 @@ import com.gameplat.admin.model.dto.MemberLoanQueryDTO;
 import com.gameplat.admin.model.dto.MemberQueryDTO;
 import com.gameplat.admin.model.vo.MemberLoanVO;
 import com.gameplat.admin.model.vo.MemberVO;
-import com.gameplat.admin.service.MemberGrowthConfigService;
-import com.gameplat.admin.service.MemberGrowthLevelService;
-import com.gameplat.admin.service.MemberLoanService;
-import com.gameplat.admin.service.MemberService;
-import com.gameplat.model.entity.member.Member;
-import com.gameplat.model.entity.member.MemberGrowthLevel;
-import com.gameplat.model.entity.member.MemberLoan;
+import com.gameplat.admin.service.*;
+import com.gameplat.base.common.context.GlobalContextHolder;
+import com.gameplat.base.common.exception.ServiceException;
+import com.gameplat.common.enums.TranTypes;
+import com.gameplat.model.entity.member.*;
+import com.gameplat.model.entity.message.Message;
 import lombok.extern.slf4j.Slf4j;
-import org.mapstruct.ap.internal.model.assignment.UpdateWrapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Isolation;
@@ -44,7 +43,13 @@ public class MemberLoanServiceImpl extends ServiceImpl<MemberLoanMapper, MemberL
     @Autowired
     private MemberService memberService;
     @Autowired
+    private MemberInfoService memberInfoService;
+    @Autowired
     private MemberGrowthLevelService memberGrowthLevelService;
+    @Autowired
+    private MemberBillService memberBillService;
+    @Autowired
+    private MessageInfoService messageInfoService;
 
     /**
      * 分页查
@@ -91,7 +96,7 @@ public class MemberLoanServiceImpl extends ServiceImpl<MemberLoanMapper, MemberL
                 .le(ObjectUtil.isNotNull(dto.getMaxOverdraftMoney()), MemberLoan::getOverdraftMoney, dto.getMaxOverdraftMoney())
                 .ge(ObjectUtil.isNotNull(dto.getBeginLoanTime()), MemberLoan::getLoanTime, dto.getBeginLoanTime())
                 .le(ObjectUtil.isNotNull(dto.getEndLoanTime()), MemberLoan::getLoanTime, dto.getEndLoanTime())
-                .eq(ObjectUtil.isNotNull(dto.getLoanStatus()), MemberLoan::getLoanTime, dto.getLoanStatus())
+                .eq(ObjectUtil.isNotNull(dto.getLoanStatus()), MemberLoan::getLoanStatus, dto.getLoanStatus())
                 .page(page)
                 .convert(memberLoanConvert::toVo);
 
@@ -103,24 +108,90 @@ public class MemberLoanServiceImpl extends ServiceImpl<MemberLoanMapper, MemberL
         MemberLoan loan = this.lambdaQuery()
                 .eq(MemberLoan::getMemberId, memberLoan.getMemberId())
                 .one();
-        if(ObjectUtil.isNotNull(loan)){
-            LambdaUpdateWrapper<MemberLoan> wrapper = new LambdaUpdateWrapper();
+        if (ObjectUtil.isNotNull(loan)) {
+            LambdaUpdateWrapper<MemberLoan> wrapper = new LambdaUpdateWrapper<>();
             wrapper.set(MemberLoan::getLoanMoney, memberLoan.getLoanMoney())
                     .eq(MemberLoan::getMemberId, memberLoan.getMemberId());
             this.update(wrapper);
-        }else{
+        } else {
             MemberVO member = memberService.queryList(new MemberQueryDTO() {{
                 setId(memberLoan.getMemberId());
             }}).get(0);
             memberLoan.setUserLevel(member.getUserLevel())
-                      .setSuperPath(member.getSuperPath())
-                      .setParentId(member.getParentId())
-                      .setParentName(member.getParentName())
-                      .setVipLevel(member.getVipLevel())
-                      .setMemberBalance(member.getBalance())
-                      .setLoanStatus(1)
-                      .setOverdraftMoney(new BigDecimal(0.00));
+                    .setSuperPath(member.getSuperPath())
+                    .setParentId(member.getParentId())
+                    .setParentName(member.getParentName())
+                    .setVipLevel(member.getVipLevel())
+                    .setMemberBalance(member.getBalance())
+                    .setLoanStatus(1)
+                    .setOverdraftMoney(new BigDecimal(0.00));
             this.save(memberLoan);
+        }
+
+    }
+
+    /**
+     * 回收
+     */
+    @Override
+    public void recycle(String idList) {
+        String username = GlobalContextHolder.getContext().getUsername();
+        String[] split = idList.split(",");
+        for (String loanId : split) {
+            Long id = Long.parseLong(loanId);
+            MemberLoan memberLoan = this.lambdaQuery()
+                    .eq(MemberLoan::getId, id)
+                    .one();
+            if (ObjectUtil.isNull(memberLoan)) {
+                throw new ServiceException("此账号暂无法使用借呗功能");
+            }
+            if (memberLoan.getLoanStatus() == 1) {
+                throw new ServiceException("此账号暂无欠款");
+            } else if (memberLoan.getLoanStatus() == 2) {
+                throw new ServiceException("此账号欠款已回收");
+            }
+            //欠款金额
+            BigDecimal overdraftMoney = memberLoan.getOverdraftMoney();
+            MemberInfo memberInfo = memberInfoService.lambdaQuery()
+                    .eq(MemberInfo::getMemberId, id)
+                    .one();
+            BigDecimal balance = memberInfo.getBalance();
+            Member member = memberService.getById(id);
+            if (balance.compareTo(overdraftMoney) < 0) {
+                throw new ServiceException(member.getAccount() + " 余额不足，扣款失败!");
+            }
+            this.updateById(new MemberLoan() {{
+                setId(id);
+                setOverdraftMoney(new BigDecimal(0.00));
+            }});
+            LambdaUpdateWrapper<MemberInfo> wrapper = new LambdaUpdateWrapper<>();
+            wrapper.set(MemberInfo::getBalance, balance.subtract(overdraftMoney))
+                    .eq(MemberInfo::getMemberId, id);
+            memberInfoService.update(wrapper);
+
+            MemberBill memberBill = new MemberBill();
+            memberBill.setMemberId(id);
+            memberBill.setAccount(member.getAccount());
+            memberBill.setMemberPath(member.getSuperPath());
+            memberBill.setTranType(TranTypes.LOAN_RECYCLE.getValue());
+            memberBill.setAmount(overdraftMoney);
+            memberBill.setBalance(balance);
+            memberBill.setContent("管理员:" + username + " 于 " + DateUtil.now() + "回收借呗欠款 " + overdraftMoney);
+            memberBill.setRemark("回收欠款");
+            memberBill.setOperator(username);
+            memberBillService.save(memberBill);
+
+            Message message = new Message();
+            message.setTitle("借呗欠款回收");
+            message.setContent("借呗欠款：" + overdraftMoney + " 已回收");
+            message.setCategory(4);
+            message.setPushRange(2);
+            message.setLinkAccount(member.getAccount());
+            message.setType(1);
+            message.setStatus(1);
+            message.setRemarks("借呗欠款回收");
+            messageInfoService.save(message);
+
         }
 
     }

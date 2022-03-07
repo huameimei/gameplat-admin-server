c95296ba011c80e07613c7b09a273989d0c1f84b
oldman <oldman>
2022-03-07 17:53:19 +0800

    红包配置数据

---
+++ src/main/java/com/gameplat/admin/controller/open/operator/RedEnvelopeInfoController.java
@@ -0,0 +1,85 @@
+package com.gameplat.admin.controller.open.operator;
+
+
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.PageDTO;
+import com.gameplat.admin.model.dto.RedEnvelopeConfigDTO;
+import com.gameplat.admin.model.dto.UserRedEnvelopeDTO;
+import com.gameplat.admin.model.vo.UserRedEnvelopeVO;
+import com.gameplat.admin.service.RedEnvelopeConfigService;
+import com.gameplat.admin.service.UserRedEnvelopeService;
+import com.gameplat.model.entity.recharge.RedEnvelopeConfig;
+import com.gameplat.model.entity.recharge.UserRedEnvelope;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/admin/operator/red")
+public class RedEnvelopeInfoController {
+
+
+    @Autowired
+    private UserRedEnvelopeService userRedEnvelopeService;
+
+    @Autowired
+    private RedEnvelopeConfigService redEnvelopeService;
+
+    /**
+     * 新增红包配置
+     */
+    @PostMapping("redAdd")
+//    @PreAuthorize("hasAuthority('operator:loginLogs:view')")
+    public Object redAdd(@RequestBody RedEnvelopeConfigDTO dto) {
+        return redEnvelopeService.redAdd(dto);
+    }
+
+    /**
+     * 红包配置列表
+     */
+    @GetMapping("/redList")
+//    @PreAuthorize("hasAuthority('operator:loginLogs:view')")
+    public Object redList(PageDTO<RedEnvelopeConfig> page, RedEnvelopeConfigDTO dto) {
+        return redEnvelopeService.redList(page,dto);
+    }
+
+    /**
+     * 红包设置
+     */
+    @PostMapping("/redEdit")
+//    @PreAuthorize("hasAuthority('operator:loginLogs:view')")
+    public Object redEdit(@RequestBody RedEnvelopeConfigDTO dto) {
+        return redEnvelopeService.redEdit(dto);
+    }
+
+    /**
+     * 红包删除
+     */
+    @DeleteMapping("/redDelete")
+//    @PreAuthorize("hasAuthority('operator:loginLogs:view')")
+    public Object redDelete(@RequestParam List<Integer> ids) {
+        return redEnvelopeService.redDelete(ids);
+    }
+
+    /**
+     * 红包记录
+     * @return
+     */
+    @GetMapping("/recordList")
+//    @PreAuthorize("hasAuthority('operator:loginLogs:view')")
+    public IPage<UserRedEnvelopeVO> recordList(UserRedEnvelopeDTO dto) {
+        return userRedEnvelopeService.recordList(dto);
+    }
+
+
+    /**
+     * 红包回收
+     */
+    @PostMapping("/redRecycle")
+    //    @PreAuthorize("hasAuthority('operator:loginLogs:view')")
+    public Object redRecycle(){return null;}
+
+}
---
+++ src/main/java/com/gameplat/admin/service/impl/RedEnvelopConfigServiceImpl.java
@@ -0,0 +1,92 @@
+package com.gameplat.admin.service.impl;
+
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.core.toolkit.ObjectUtils;
+import com.baomidou.mybatisplus.extension.plugins.pagination.PageDTO;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.gameplat.admin.convert.RedEnvelopeConfigConvert;
+import com.gameplat.admin.mapper.RedEnvelopConfigMapper;
+import com.gameplat.admin.model.dto.RedEnvelopeConfigDTO;
+import com.gameplat.admin.service.RedEnvelopeConfigService;
+import com.gameplat.model.entity.recharge.RedEnvelopeConfig;
+import com.gameplat.model.entity.spread.SpreadUnion;
+import com.gameplat.security.SecurityUserHolder;
+import com.gameplat.security.context.UserCredential;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Date;
+import java.util.List;
+
+@Service
+@Transactional(isolation = Isolation.DEFAULT, rollbackFor = Throwable.class)
+public class RedEnvelopConfigServiceImpl extends ServiceImpl<RedEnvelopConfigMapper, RedEnvelopeConfig>
+        implements RedEnvelopeConfigService {
+
+  @Autowired
+  private RedEnvelopeConfigConvert redConvert;
+
+  /**
+   * 添加红包配置
+   */
+  @Override
+  public boolean redAdd(RedEnvelopeConfigDTO dto) {
+    UserCredential credential = SecurityUserHolder.getCredential();
+    dto.setCreateBy(credential.getUsername());
+    return this.save(redConvert.dtoToEntity(dto));
+  }
+
+  /**
+   * 获取红包列表
+   */
+  @Override
+  public IPage<RedEnvelopeConfig> redList(PageDTO<RedEnvelopeConfig> page, RedEnvelopeConfigDTO dto) {
+    return this.lambdaQuery()
+            .like(ObjectUtils.isNotEmpty(dto.getRedName()),RedEnvelopeConfig::getRedName,dto.getRedName())
+            .eq(ObjectUtils.isNotEmpty(dto.getIsAging()),RedEnvelopeConfig::getIsAging,dto.getIsAging())
+            .eq(ObjectUtils.isNotEmpty(dto.getState()),RedEnvelopeConfig::getState,dto.getState())
+            .eq(ObjectUtils.isNotEmpty(dto.getReceiveMethod()),RedEnvelopeConfig::getReceiveMethod,dto.getReceiveMethod())
+            .eq(ObjectUtils.isNotEmpty(dto.getReceiveStartTime()),RedEnvelopeConfig::getReceiveStartTime,dto.getReceiveStartTime())
+            .eq(ObjectUtils.isNotEmpty(dto.getReceiveEndTime()),RedEnvelopeConfig::getReceiveEndTime,dto.getReceiveEndTime())
+            .orderByDesc(RedEnvelopeConfig::getCreateTime)
+            .orderByDesc(RedEnvelopeConfig::getUpdateTime)
+            .page(page);
+  }
+
+  /**
+   * 修改红包配置
+   */
+  @Override
+  public boolean redEdit(RedEnvelopeConfigDTO dto) {
+    UserCredential credential = SecurityUserHolder.getCredential();
+    dto.setUpdateBy(credential.getUsername());
+    return this.lambdaUpdate()
+            .set(ObjectUtils.isNotNull(dto.getRedName()), RedEnvelopeConfig::getRedName, dto.getRedName())
+            .set(ObjectUtils.isNotNull(dto.getAmount()), RedEnvelopeConfig::getAmount, dto.getAmount())
+            .set(ObjectUtils.isNotNull(dto.getMultiple()), RedEnvelopeConfig::getMultiple, dto.getMultiple())
+            .set(ObjectUtils.isNotNull(dto.getIsAging()), RedEnvelopeConfig::getIsAging, dto.getIsAging())
+            .set(ObjectUtils.isNotNull(dto.getReceiveStartTime()), RedEnvelopeConfig::getReceiveStartTime, dto.getReceiveStartTime())
+            .set(ObjectUtils.isNotNull(dto.getReceiveEndTime()), RedEnvelopeConfig::getReceiveEndTime, dto.getReceiveEndTime())
+            .set(ObjectUtils.isNotNull(dto.getRechargeAmount()), RedEnvelopeConfig::getRechargeAmount, dto.getRechargeAmount())
+            .set(ObjectUtils.isNotNull(dto.getChipRequire()), RedEnvelopeConfig::getChipRequire, dto.getChipRequire())
+            .set(ObjectUtils.isNotNull(dto.getReceiveMethod()), RedEnvelopeConfig::getReceiveMethod, dto.getReceiveMethod())
+            .set(ObjectUtils.isNotNull(dto.getImgUrl()), RedEnvelopeConfig::getImgUrl, dto.getImgUrl())
+            .set(ObjectUtils.isNotNull(dto.getLocation()), RedEnvelopeConfig::getLocation, dto.getLocation())
+            .set(ObjectUtils.isNotNull(dto.getRemark()), RedEnvelopeConfig::getRemark, dto.getRemark())
+            .set(ObjectUtils.isNotNull(dto.getUpdateBy()), RedEnvelopeConfig::getUpdateBy, dto.getUpdateBy())
+            .set(RedEnvelopeConfig::getUpdateTime,new Date())
+            .eq(ObjectUtils.isNotNull(), RedEnvelopeConfig::getId, dto.getId()).update();
+  }
+
+  /**
+   * 删除红包配置
+   */
+  @Override
+  public boolean redDelete(List<Integer> ids) {
+    return this.removeByIds(ids);
+  }
+
+
+}

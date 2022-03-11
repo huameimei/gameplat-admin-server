550bb3f652957530eaa831c173a37909e4549c99
hoover <hoover@kg.com>
2022-03-11 17:52:50 +0400

    指定返回数据字段

--- src/main/java/com/gameplat/admin/service/impl/TpInterfaceServiceImpl.java
+++ src/main/java/com/gameplat/admin/service/impl/TpInterfaceServiceImpl.java
@@ -36,7 +36,8 @@ public class TpInterfaceServiceImpl extends ServiceImpl<TpInterfaceMapper, TpInt
 
   @Override
   public List<TpInterfaceVO> queryAll() {
-    return this.list().stream().map(tpInterfaceConvert::toVo).collect(Collectors.toList());
+    return this.lambdaQuery().select(TpInterface::getId,TpInterface::getCode,TpInterface::getName)
+            .list().stream().map(tpInterfaceConvert::toVo).collect(Collectors.toList());
   }
 
   @Override

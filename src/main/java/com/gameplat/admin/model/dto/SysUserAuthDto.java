c0921595199971138a6f6f1a6f5099bad5a5c2a3
kb <kb@qq.com>
2022-03-12 18:01:41 +0800

    用户认证

---
+++ src/main/java/com/gameplat/admin/model/dto/SysUserAuthDto.java
@@ -0,0 +1,43 @@
+package com.gameplat.admin.model.dto;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * @Author kb
+ * @Date 2022/3/12 17:14
+ * @Version 1.0
+ */
+@Data
+public class SysUserAuthDto implements Serializable {
+
+    @ApiModelProperty(value = "主键")
+    private Integer authId;
+
+    @ApiModelProperty(value = "身份认证中心url")
+    private String idcardAuthUrl;
+
+    @ApiModelProperty(value = "身份认证中心key")
+    private String idcardAuthKey;
+
+    @ApiModelProperty(value = "银行认证中心url")
+    private String bankAuthUrl;
+
+    @ApiModelProperty(value = "银行认证中心key")
+    private String bankAuthKey;
+
+    @ApiModelProperty(value = "创建人")
+    private String createBy;
+
+    @ApiModelProperty(value = "创建时间")
+    private Date createTime;
+
+    @ApiModelProperty(value = "更新人")
+    private String updateBy;
+
+    @ApiModelProperty(value = "更新时间")
+    private Date updateTime;
+}
---
+++ src/main/java/com/gameplat/admin/model/vo/SysUserAuthVo.java
@@ -0,0 +1,30 @@
+package com.gameplat.admin.model.vo;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import java.io.Serializable;
+
+/**
+ * @Author kb
+ * @Date 2022/3/12 17:14
+ * @Version 1.0
+ */
+@Data
+public class SysUserAuthVo implements Serializable {
+
+
+    @ApiModelProperty(value = "主键")
+    private Integer authId;
+
+    @ApiModelProperty(value = "身份认证中心url")
+    private String idcardAuthUrl;
+
+    @ApiModelProperty(value = "身份认证中心key")
+    private String idcardAuthKey;
+
+    @ApiModelProperty(value = "银行认证中心url")
+    private String bankAuthUrl;
+
+    @ApiModelProperty(value = "银行认证中心key")
+    private String bankAuthKey;
+}

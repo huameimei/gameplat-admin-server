026f816f2d21df1a7a838377360f930ad280fd3d
robben <>
2022-03-13 20:21:12 +0400

    优化密码错误次数

--- src/main/java/com/gameplat/admin/cache/AdminCache.java
+++ src/main/java/com/gameplat/admin/cache/AdminCache.java
@@ -1,12 +1,15 @@
 package com.gameplat.admin.cache;
 
-import com.gameplat.base.common.constant.RedisCacheKey;
+import com.gameplat.admin.service.LimitInfoService;
+import com.gameplat.common.constant.CachedKeys;
+import com.gameplat.common.model.bean.limit.AdminLoginLimit;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 
 import java.util.Optional;
+import java.util.concurrent.TimeUnit;
 
 /**
  * 后台账号Cache操作
@@ -19,6 +22,8 @@ public class AdminCache {
 
   @Autowired private RedisTemplate<String, Integer> redisTemplate;
 
+  @Autowired private LimitInfoService limitInfoService;
+
   /**
    * 获取后台登录错误次数
    *
@@ -26,15 +31,24 @@ public class AdminCache {
    * @return int
    */
   public int getErrorPasswordCount(String account) {
-    Integer count = redisTemplate.opsForValue().get(RedisCacheKey.ADMIN_PWDERROR_KEY + account);
+    Integer count = redisTemplate.opsForValue().get(CachedKeys.ADMIN_PWD_ERROR_COUNT + account);
     return Optional.ofNullable(count).orElse(0);
   }
 
-  public void updateErrorPasswordCount(String account, int count) {
-    redisTemplate.opsForValue().increment(RedisCacheKey.ADMIN_PWDERROR_KEY + account, count);
+  public void updateErrorPasswordCount(String account) {
+    AdminLoginLimit limit = limitInfoService.getAdminLimit();
+    Integer pwdErrorCount = limit.getPwdErrorCount();
+    if (null == pwdErrorCount || pwdErrorCount <= 0) {
+      return;
+    }
+
+    // 更新密码错误次数
+    String key = CachedKeys.ADMIN_PWD_ERROR_COUNT + account;
+    redisTemplate.opsForValue().increment(key);
+    redisTemplate.expire(key, 1, TimeUnit.DAYS);
   }
 
   public void cleanErrorPasswordCount(String account) {
-    redisTemplate.delete(RedisCacheKey.ADMIN_PWDERROR_KEY + account);
+    redisTemplate.delete(CachedKeys.ADMIN_PWD_ERROR_COUNT + account);
   }
 }

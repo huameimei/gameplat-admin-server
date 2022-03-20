e0c835e6a9c8719ae1365c283474aa4a45045ea7
lily <@@@sq125>
2022-03-20 15:51:44 +0400

    删除无用账变类型枚举类

--- src/main/java/com/gameplat/admin/controller/open/member/MemberGoldCoinRecordController.java
+++ src/main/java/com/gameplat/admin/controller/open/member/MemberGoldCoinRecordController.java
@@ -22,7 +22,6 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author lily

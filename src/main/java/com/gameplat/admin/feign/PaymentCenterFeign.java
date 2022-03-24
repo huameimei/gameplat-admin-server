031136e88d7d097885669102dbb8921131702870
jakie <111111@jakie.com>
2022-03-24 19:49:53 +0400

    调整支付中心fegin

--- src/main/java/com/gameplat/admin/feign/PaymentCenterFeign.java
+++ src/main/java/com/gameplat/admin/feign/PaymentCenterFeign.java
@@ -5,13 +5,14 @@ import com.gameplat.base.common.web.Result;
 import com.gameplat.basepay.proxypay.thirdparty.ProxyCallbackContext;
 import com.gameplat.basepay.proxypay.thirdparty.ProxyDispatchContext;
 import com.gameplat.basepay.proxypay.thirdparty.ProxyPayBackResult;
+import com.gameplat.common.game.config.FeignRestConfig;
 import com.gameplat.web.config.feign.FeignClientConfig;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(value = "payment-service", configuration = FeignClientConfig.class)
+@FeignClient(value = "payment-service", configuration = FeignRestConfig.class)
 public interface PaymentCenterFeign {
 
   @PostMapping("/api/internal/proxyPay/onlineProxyPay/{code}/{name}")

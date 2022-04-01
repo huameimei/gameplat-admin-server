2414c8bef413a768c579161a83052874aa5dbf78
lily <@@@sq125>
2022-04-01 10:03:58 +0400

    权限调整

--- src/main/java/com/gameplat/admin/controller/open/member/MemberController.java
+++ src/main/java/com/gameplat/admin/controller/open/member/MemberController.java
@@ -217,6 +217,7 @@ public class MemberController {
    */
   @ApiOperation(value = "批量更改日工资")
   @PostMapping("/updateDaySalary")
+  @PreAuthorize("hasAuthority('member:updateDaySalary')")
   @Log(module = ServiceName.ADMIN_SERVICE, type = LogType.MEMBER, desc = "修改会员工资状态")
   public void updateDaySalary(
       @RequestParam(required = true) String ids, @RequestParam(required = true) Integer state) {

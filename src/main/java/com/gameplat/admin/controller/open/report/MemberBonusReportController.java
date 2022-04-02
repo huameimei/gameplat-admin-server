fd0a51c67c5145123fae36cf79248cd0cde4c8fe
peter <peter168168168@outlook.com>
2022-04-02 19:29:04 +0800

    权限配置

--- src/main/java/com/gameplat/admin/controller/open/report/MemberBonusReportController.java
+++ src/main/java/com/gameplat/admin/controller/open/report/MemberBonusReportController.java
@@ -11,14 +11,13 @@ import com.gameplat.common.constant.ServiceName;
 import com.gameplat.log.annotation.Log;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import java.util.Date;
+import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.servlet.http.HttpServletResponse;
-import java.util.Date;
 
 /**
  * @author aBen
@@ -30,15 +29,15 @@ import java.util.Date;
 @RequestMapping("/api/admin/report/bonus")
 public class MemberBonusReportController {
 
-  @Autowired
-  private MemberBonusReportService memberBonusReportService;
+  @Autowired private MemberBonusReportService memberBonusReportService;
 
   @GetMapping(value = "/findReportPage")
   @PreAuthorize("hasAuthority('report:dividendtDataReport:view')")
   @ApiOperation(value = "查询会员红利报表")
-  @PreAuthorize("hasAuthority('bonus:report:page')")
-  public PageDtoVO<MemberBonusReportVO> findReportPage(PageDTO<MemberBonusReportVO> page, MemberBonusReportQueryDTO queryDTO) {
-    if (StringUtils.isEmpty(queryDTO.getStartTime()) || StringUtils.isEmpty(queryDTO.getEndTime())) {
+  public PageDtoVO<MemberBonusReportVO> findReportPage(
+      PageDTO<MemberBonusReportVO> page, MemberBonusReportQueryDTO queryDTO) {
+    if (StringUtils.isEmpty(queryDTO.getStartTime())
+        || StringUtils.isEmpty(queryDTO.getEndTime())) {
       queryDTO.setStartTime(DateUtil.format(new Date(), "YYYY-MM-dd"));
       queryDTO.setEndTime(DateUtil.format(new Date(), "YYYY-MM-dd"));
     }
@@ -50,11 +49,11 @@ public class MemberBonusReportController {
   @PreAuthorize("hasAuthority('bonus:report:export')")
   @Log(module = ServiceName.ADMIN_SERVICE, desc = "导出会员红利报表")
   public void exportReport(MemberBonusReportQueryDTO queryDTO, HttpServletResponse response) {
-    if (StringUtils.isEmpty(queryDTO.getStartTime()) || StringUtils.isEmpty(queryDTO.getEndTime())) {
+    if (StringUtils.isEmpty(queryDTO.getStartTime())
+        || StringUtils.isEmpty(queryDTO.getEndTime())) {
       queryDTO.setStartTime(DateUtil.format(new Date(), "YYYY-MM-dd"));
       queryDTO.setEndTime(DateUtil.format(new Date(), "YYYY-MM-dd"));
     }
     memberBonusReportService.exportMemberBonusReport(queryDTO, response);
   }
-
 }

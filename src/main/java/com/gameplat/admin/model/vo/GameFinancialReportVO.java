cd4a4d14c29d42929f51c5930007ad063b19f68c
aben <aben@kg.com>
2022-04-08 18:33:48 +0800

    1

--- src/main/java/com/gameplat/admin/model/vo/GameFinancialReportVO.java
+++ src/main/java/com/gameplat/admin/model/vo/GameFinancialReportVO.java
@@ -1,5 +1,6 @@
 package com.gameplat.admin.model.vo;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
@@ -64,6 +65,7 @@ public class GameFinancialReportVO implements Serializable {
   private String endTime;
 
   @ApiModelProperty(value = "创建时间")
+  @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
   private Date createTime;
 
   @ApiModelProperty(value = "创建人")

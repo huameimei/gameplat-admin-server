0c772a585f8e4c43849d743023d17d8b9c5bfbeb
kb <kb@qq.com>
2022-04-24 00:03:40 +0800

    接口权限处理

--- src/main/java/com/gameplat/admin/controller/open/system/OpenLimitInfoController.java
+++ src/main/java/com/gameplat/admin/controller/open/system/OpenLimitInfoController.java
@@ -15,17 +15,129 @@ public class OpenLimitInfoController {
 
   @Autowired private LimitInfoService limitInfoService;
 
-  /** 添加/修改配置项 */
-  @PutMapping(value = "/add")
-  @PreAuthorize("hasAuthority('system:limit:add')")
-  public void save(@RequestBody LimitInfoDTO dto) {
+  /**
+   * 登录后台限制添加/修改配置项
+   */
+  @PutMapping(value = "/add/adminLoginLimit")
+  @PreAuthorize("hasAuthority('system:limit:add:adminLoginLimit')")
+  public void saveAdminLoginLimit(@RequestBody LimitInfoDTO dto) {
     limitInfoService.insertLimitInfo(dto);
   }
 
-  /** 根据名称获取配置 */
-  @GetMapping(value = "/get/{name}")
-  @PreAuthorize("hasAuthority('system:limit:view')")
-  public LimitInfo get(@PathVariable String name) {
+  /**
+   * 根据名称获取配置登录后台限制
+   */
+  @GetMapping(value = "/get/adminLoginLimit")
+  @PreAuthorize("hasAuthority('system:limit:view:adminLoginLimit')")
+  public LimitInfo getAdminLoginLimit(String name) {
+    return limitInfoService.getLimitInfo(name);
+  }
+
+  /**
+   * 完善资料添加/修改配置项
+   */
+  @PutMapping(value = "/add/editUserInfoLimit")
+  @PreAuthorize("hasAuthority('system:limit:addUserInfoLimit')")
+  public void saveEditUserInfoLimit(@RequestBody LimitInfoDTO dto) {
+    limitInfoService.insertLimitInfo(dto);
+  }
+
+  /**
+   * 根据名称获取配置完善资料
+   */
+  @GetMapping(value = "/get/editUserInfoLimit")
+  @PreAuthorize("hasAuthority('system:limit:viewUserInfoLimit')")
+  public LimitInfo getEditUserInfoLimit(String name) {
+    return limitInfoService.getLimitInfo(name);
+  }
+
+  /**
+   * 真人额度转换配置限制添加/修改配置项
+   */
+  @PutMapping(value = "/add/liveTransferLimit")
+  @PreAuthorize("hasAuthority('system:limit:addTransferLimit')")
+  public void saveLiveTransferLimit(@RequestBody LimitInfoDTO dto) {
+    limitInfoService.insertLimitInfo(dto);
+  }
+
+  /**
+   * 根据名称获取配置真人额度转换配置
+   */
+  @GetMapping(value = "/get/liveTransferLimit")
+  @PreAuthorize("hasAuthority('system:limit:viewTransferLimit')")
+  public LimitInfo getLiveTransferLimit(String name) {
+    return limitInfoService.getLimitInfo(name);
+  }
+
+  /**
+   * 会员登录配置限制添加/修改配置项
+   */
+  @PutMapping(value = "/add/memberLoginLimit")
+  @PreAuthorize("hasAuthority('system:limit:addLoginLimit')")
+  public void saveMemberLoginLimit(@RequestBody LimitInfoDTO dto) {
+    limitInfoService.insertLimitInfo(dto);
+  }
+
+  /**
+   * 根据名称获取配置会员登录配置限制
+   */
+  @GetMapping(value = "/get/memberLoginLimit")
+  @PreAuthorize("hasAuthority('system:limit:viewLoginLimit')")
+  public LimitInfo getMemberLoginLimit(String name) {
+    return limitInfoService.getLimitInfo(name);
+  }
+
+  /**
+   * 会员充值限制添加/修改配置项
+   */
+  @PutMapping(value = "/add/memberRechargeLimit")
+  @PreAuthorize("hasAuthority('system:limit:addRechargeLimit')")
+  public void saveMemberRechargeLimit(@RequestBody LimitInfoDTO dto) {
+    limitInfoService.insertLimitInfo(dto);
+  }
+
+  /**
+   * 根据名称获取配置会员充值限制
+   */
+  @GetMapping(value = "/get/memberRechargeLimit")
+  @PreAuthorize("hasAuthority('system:limit:viewRechargeLimit')")
+  public LimitInfo getMemberRechargeLimit(String name) {
+    return limitInfoService.getLimitInfo(name);
+  }
+
+  /**
+   * 会员注册配置限制添加/修改配置项
+   */
+  @PutMapping(value = "/add/memberRegistryLimit")
+  @PreAuthorize("hasAuthority('system:limit:addRegistryLimit')")
+  public void saveMemberRegistryLimit(@RequestBody LimitInfoDTO dto) {
+    limitInfoService.insertLimitInfo(dto);
+  }
+
+  /**
+   * 根据名称获取配置注册限制
+   */
+  @GetMapping(value = "/get/memberRegistryLimit")
+  @PreAuthorize("hasAuthority('system:limit:viewRegistryLimit')")
+  public LimitInfo getMemberRegistryLimit(String name) {
+    return limitInfoService.getLimitInfo(name);
+  }
+
+  /**
+   * 会员提现配置限制添加/修改配置项
+   */
+  @PutMapping(value = "/add/memberWithdrawLimit")
+  @PreAuthorize("hasAuthority('system:limit:addWithdrawLimit')")
+  public void saveMemberWithdrawLimit(@RequestBody LimitInfoDTO dto) {
+    limitInfoService.insertLimitInfo(dto);
+  }
+
+  /**
+   * 根据名称获取配置会员提现配置限制
+   */
+  @GetMapping(value = "/get/memberWithdrawLimit")
+  @PreAuthorize("hasAuthority('system:limit:viewWithdrawLimit')")
+  public LimitInfo getMemberWithdrawLimit(String name) {
     return limitInfoService.getLimitInfo(name);
   }
 }

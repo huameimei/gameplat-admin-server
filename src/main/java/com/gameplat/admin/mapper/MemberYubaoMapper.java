daabcf390b51940b4cc5d02368343abf8de4f838
ALAN <alan@kg.com>
2022-05-12 19:22:34 +0800

    余额宝后台接口

---
+++ src/main/java/com/gameplat/admin/mapper/MemberYubaoMapper.java
@@ -0,0 +1,8 @@
+package com.gameplat.admin.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.gameplat.model.entity.member.MemberYubao;
+
+public interface MemberYubaoMapper extends BaseMapper<MemberYubao> {
+
+}

c8e3c3d0e8418b7c66e9d6f0ccdd814fdeb11416
peter <peter168168168@outlook.com>
2022-05-12 18:34:27 +0800

    投注记录导出

--- src/main/java/com/gameplat/admin/controller/open/game/GameBetDailyReportController.java
+++ src/main/java/com/gameplat/admin/controller/open/game/GameBetDailyReportController.java
@@ -20,14 +20,17 @@ import com.gameplat.model.entity.game.GameBetDailyReport;
 import com.gameplat.model.entity.game.GamePlatform;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import java.util.Date;
+import java.util.List;
+import javax.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
-
-import javax.validation.Valid;
-import java.util.Date;
-import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @Api(tags = "游戏日报表")

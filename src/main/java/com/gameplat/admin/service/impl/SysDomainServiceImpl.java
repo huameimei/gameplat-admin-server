d40be75b11b596c7b9d2f45ba6c7ac57aedfc959
robben <>
2022-05-13 11:19:03 +0400

    优化文件上传

--- src/main/java/com/gameplat/admin/service/impl/SysDomainServiceImpl.java
+++ src/main/java/com/gameplat/admin/service/impl/SysDomainServiceImpl.java
@@ -3,6 +3,7 @@ package com.gameplat.admin.service.impl;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.gameplat.admin.mapper.SysDomainMapper;
 import com.gameplat.admin.service.SysDomainService;
+import com.gameplat.base.common.exception.ServiceException;
 import com.gameplat.model.entity.setting.SysDomain;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -21,13 +22,21 @@ public class SysDomainServiceImpl extends ServiceImpl<SysDomainMapper, SysDomain
 
   @Override
   public String getChatDomain() {
-    SysDomain sysDomain =
-            lambdaQuery().eq(SysDomain::getDomainType, "chat_api_domain").one();
+    SysDomain sysDomain = lambdaQuery().eq(SysDomain::getDomainType, "chat_api_domain").one();
     String chatDomain = sysDomain.getDomain();
     if (chatDomain.endsWith("/")) {
       return chatDomain.substring(0, chatDomain.length() - 1);
     } else {
       return chatDomain;
     }
+  }
+
+  @Override
+  public String getOssDomain() {
+    return lambdaQuery()
+        .eq(SysDomain::getDomainType, "oss_domain")
+        .oneOpt()
+        .map(SysDomain::getDomain)
+        .orElseThrow(() -> new ServiceException("OSS访问地址未配置"));
   }
 }

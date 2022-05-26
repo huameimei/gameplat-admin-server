9cc5fdabf0953baa4312dad19593da31d764a56e
kb <kb@qq.com>
2022-05-26 20:43:34 +0800

    出入款添加谷歌验证码限制

--- src/main/java/com/gameplat/admin/mapper/TpMerchantMapper.java
+++ src/main/java/com/gameplat/admin/mapper/TpMerchantMapper.java
@@ -5,10 +5,11 @@ import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.gameplat.admin.model.vo.TpMerchantVO;
 import com.gameplat.model.entity.pay.TpMerchant;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TpMerchantMapper extends BaseMapper<TpMerchant> {
 
-  IPage<TpMerchantVO> findTpMerchantPage(Page<TpMerchant> page, Integer status, String name);
+  IPage<TpMerchantVO> findTpMerchantPage(Page<TpMerchant> page, @Param("status") Integer status, @Param("name") String name);
 }

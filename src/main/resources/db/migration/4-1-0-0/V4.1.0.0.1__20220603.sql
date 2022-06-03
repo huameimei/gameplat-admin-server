6c1da755b4de596c3fc36c52f8716b1d7711ecaa
cc <544765885@qq.com>
2022-06-03 22:24:01 +0800

    lily脚本

--- src/main/resources/db/migration/4-1-0-0/V4.1.0.0.1__20220603.sql
+++ src/main/resources/db/migration/4-1-0-0/V4.1.0.0.1__20220603.sql
@@ -1,1 +1,7 @@
 ALTER TABLE `activity_turntable` ADD UNIQUE (`activity_lobby_id`);
+
+alter table member modify column game_account varchar(20);
+alter table member_loan modify member_balance decimal(20,1) not null DEFAULT(0.0) COMMENT "借款金额/还款金额/回收金额";
+alter table member_loan modify loan_money decimal(20,1) not null DEFAULT(0.0) COMMENT "借呗额度";
+alter table member_loan modify overdraft_money decimal(20,1) not null DEFAULT(0.0) COMMENT "总欠款金额";
+alter table member_loan modify remain_money decimal(20,1) not null DEFAULT(0.0) COMMENT "剩余未还";

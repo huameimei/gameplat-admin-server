fa6da0bf7a71b2a4038c35eae9847f3febc37c88
aguai <ddmfony@gmail.com>
2022-06-14 19:25:15 +0800

    更新脚本

--- src/main/resources/db/migration/4-1-0-0/V4.1.0.0.1__20220603.sql
+++ src/main/resources/db/migration/4-1-0-0/V4.1.0.0.1__20220603.sql
@@ -5,3 +5,5 @@ alter table member_loan modify member_balance decimal(20,1) not null DEFAULT(0.0
 alter table member_loan modify loan_money decimal(20,1) not null DEFAULT(0.0) COMMENT "借呗额度";
 alter table member_loan modify overdraft_money decimal(20,1) not null DEFAULT(0.0) COMMENT "总欠款金额";
 alter table member_loan modify remain_money decimal(20,1) not null DEFAULT(0.0) COMMENT "剩余未还";
+
+update sys_dict_data set dict_value = 60 where dict_type = "TRANS_TYPE" and dict_label = "LOAN_REPAY";

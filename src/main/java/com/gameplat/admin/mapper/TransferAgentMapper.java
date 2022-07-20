fc5fcdca047205544670c18e638b4810282e5f6c
cc <544765885@qq.com>
2022-07-20 23:23:49 +0800

    转代理转移数据以及清除分红配置

---
+++ src/main/java/com/gameplat/admin/mapper/TransferAgentMapper.java
@@ -0,0 +1,32 @@
+package com.gameplat.admin.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+@Mapper
+public interface TransferAgentMapper {
+    Integer transferActivityRecord(@Param("account") String account,
+                                @Param("originSuperPath") String originSuperPath);
+
+    Integer transferGameReport(@Param("account") String account,
+                            @Param("originSuperPath") String originSuperPath);
+
+    Integer transferMemberReport(@Param("account") String account,
+                              @Param("originSuperPath") String originSuperPath);
+
+    Integer transferMemberBill(@Param("account") String account,
+                            @Param("originSuperPath") String originSuperPath);
+
+    Integer transferRechargeRecord(@Param("account") String account,
+                                @Param("originSuperPath") String originSuperPath);
+
+    Integer transferWithdrawRecord(@Param("account") String account,
+                                @Param("originSuperPath") String originSuperPath);
+
+    Integer transferGameRebateReport(@Param("account") String account,
+                                  @Param("originSuperPath") String originSuperPath);
+
+    Integer transferWealRecord(@Param("account") String account,
+                            @Param("originSuperPath") String originSuperPath);
+
+}

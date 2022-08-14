3bb9115c189287dec5f9eee9f1d7bd17d9cb91f6
cc <544765885@qq.com>
2022-08-14 22:21:45 +0800

    excel导入外部站点数据

---
+++ src/main/java/com/gameplat/admin/model/vo/ExternalDataVo.java
@@ -0,0 +1,9 @@
+package com.gameplat.admin.model.vo;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class ExternalDataVo implements Serializable {
+}
---
+++ src/main/java/com/gameplat/admin/service/impl/ExternalDataServiceImpl.java
@@ -0,0 +1,37 @@
+package com.gameplat.admin.service.impl;
+
+import com.gameplat.admin.model.vo.ExternalDataVo;
+import com.gameplat.admin.service.ExternalDataService;
+import com.gameplat.base.common.util.EasyExcelUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.util.List;
+
+@Slf4j
+@Service
+@Transactional(isolation = Isolation.DEFAULT, rollbackFor = Throwable.class)
+public class ExternalDataServiceImpl implements ExternalDataService {
+    /**
+     * 开始处理外部数据导入
+     * @param username
+     * @param file
+     * @param request
+     */
+    @Override
+    public void dealData(String username, MultipartFile file, HttpServletRequest request) {
+        //1. 解析Excel
+        try {
+            List<ExternalDataVo> paramlist = EasyExcelUtil.readExcel(file.getInputStream(), ExternalDataVo.class);
+            log.info("共需导入{}条数据", paramlist.size());
+
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        }
+    }
+}

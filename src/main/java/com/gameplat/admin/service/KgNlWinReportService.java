7000e49d73d90b2bcb3526f6223a1bfd6fbac108
aben <aben@kg.com>
2022-08-15 22:18:38 +0800

    彩票输赢报表

--- src/main/java/com/gameplat/admin/service/KgNlWinReportService.java
+++ src/main/java/com/gameplat/admin/service/KgNlWinReportService.java
@@ -1,7 +1,10 @@
 package com.gameplat.admin.service;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.gameplat.admin.model.dto.KgNlWinReportQueryDTO;
+import com.gameplat.admin.model.vo.KgNlBetDailyDetailVO;
 import com.gameplat.admin.model.vo.KgNlWinReportVO;
+import com.gameplat.admin.model.vo.PageDtoVO;
 
 import javax.servlet.http.HttpServletResponse;
 import java.util.List;
@@ -16,4 +19,8 @@ public interface KgNlWinReportService {
   List<KgNlWinReportVO> findList(KgNlWinReportQueryDTO queryDTO);
 
   void exportReport(KgNlWinReportQueryDTO queryDTO, HttpServletResponse response);
+
+  PageDtoVO<KgNlBetDailyDetailVO> findUserDetail(Page<KgNlBetDailyDetailVO> page, KgNlWinReportQueryDTO dto);
+
+  void exportUserDetail(KgNlWinReportQueryDTO queryDTO, HttpServletResponse response);
 }
--- src/main/java/com/gameplat/admin/service/impl/KgNlBetDailyDetailServiceImpl.java
+++ src/main/java/com/gameplat/admin/service/impl/KgNlBetDailyDetailServiceImpl.java
@@ -1,17 +1,29 @@
 package com.gameplat.admin.service.impl;
 
+import cn.afterturn.easypoi.excel.ExcelExportUtil;
+import cn.afterturn.easypoi.excel.entity.ExportParams;
+import cn.hutool.core.util.ObjectUtil;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.gameplat.admin.mapper.KgNlBetDailyDetailMapper;
 import com.gameplat.admin.model.dto.KgNlWinReportQueryDTO;
+import com.gameplat.admin.model.vo.KgNlBetDailyDetailVO;
 import com.gameplat.admin.model.vo.KgNlWinReportVO;
+import com.gameplat.admin.model.vo.PageDtoVO;
 import com.gameplat.admin.service.KgNlBetDailyDetailService;
 import com.gameplat.model.entity.game.KgNlBetDailyDetail;
+import org.apache.poi.ss.usermodel.Workbook;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 @Transactional(isolation = Isolation.DEFAULT, rollbackFor = Throwable.class)
@@ -21,7 +33,37 @@ public class KgNlBetDailyDetailServiceImpl extends ServiceImpl<KgNlBetDailyDetai
   @Autowired
   private KgNlBetDailyDetailMapper kgNlBetDailyDetailMapper;
 
-  public List<KgNlWinReportVO> findWinReportData(KgNlWinReportQueryDTO dto){
+  public List<KgNlWinReportVO> findWinReportData(KgNlWinReportQueryDTO dto) {
     return kgNlBetDailyDetailMapper.findWinReportData(dto);
+  }
+
+  public PageDtoVO<KgNlBetDailyDetailVO> findDetailPage(Page<KgNlBetDailyDetailVO> page, KgNlWinReportQueryDTO dto) {
+    PageDtoVO<KgNlBetDailyDetailVO> pageDtoVO = new PageDtoVO<>();
+    Page<KgNlBetDailyDetailVO> list = kgNlBetDailyDetailMapper.findDetailPage(page, dto);
+    if (ObjectUtil.isEmpty(list)) {
+      return pageDtoVO;
+    }
+
+    // 查询总计
+    KgNlBetDailyDetail totalData = kgNlBetDailyDetailMapper.findTotalData(dto);
+
+    Map<String, Object> otherData = new HashMap<>();
+    otherData.put("totalData", totalData);
+    pageDtoVO.setPage(list);
+    pageDtoVO.setOtherData(otherData);
+    return pageDtoVO;
+  }
+
+  @Override
+  public void exportDetail(HttpServletResponse response, KgNlWinReportQueryDTO dto) {
+    String title = dto.getBeginTime() + "-" + dto.getEndTime() + " 彩票输赢详情";
+    ExportParams exportParams = new ExportParams(title, "彩票输赢详情");
+    response.setHeader(HttpHeaders.CONTENT_DISPOSITION, "attachment;filename = vipWealReword.xls");
+    List<KgNlBetDailyDetailVO> list = kgNlBetDailyDetailMapper.findDetailList(dto);
+    try (Workbook workbook = ExcelExportUtil.exportExcel(exportParams, KgNlBetDailyDetailVO.class, list)) {
+      workbook.write(response.getOutputStream());
+    } catch (IOException e) {
+      log.error("彩票输赢详情导出IO异常", e);
+    }
   }
 }

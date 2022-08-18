a62a15f132ba6d3ef87cc55d67996bffe41490ad
aguai <ddmfony@gmail.com>
2022-08-19 03:09:17 +0800

    定时任务服务

--- src/main/java/com/gameplat/admin/service/impl/TransferAgentServiceImpl.java
+++ src/main/java/com/gameplat/admin/service/impl/TransferAgentServiceImpl.java
@@ -126,7 +126,7 @@ public class TransferAgentServiceImpl implements TransferAgentService {
             elasticsearchTemplate.save(
                 listRecord, IndexCoordinates.of(ContextConstant.ES_INDEX.BET_RECORD_ + tenantCode));
           }
-          Thread.sleep(200);
+          Thread.sleep(250);
         }
       } while (CollectionUtil.isNotEmpty(resultList));
     } catch (Exception e) {

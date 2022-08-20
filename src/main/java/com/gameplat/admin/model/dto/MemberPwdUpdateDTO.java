00b7a78e34572470d6f83b7664a56333fe0731ed
kb <kb@qq.com>
2022-08-20 17:30:12 +0800

    重置提现密码

--- src/main/java/com/gameplat/admin/model/dto/MemberPwdUpdateDTO.java
+++ src/main/java/com/gameplat/admin/model/dto/MemberPwdUpdateDTO.java
@@ -12,6 +12,7 @@ public class MemberPwdUpdateDTO implements Serializable {
   @NotNull(message = "会员编号不能为空")
   private Long id;
 
+  @NotEmpty(message = "登录密码不能为空")
   private String password;
 
   private String remark;

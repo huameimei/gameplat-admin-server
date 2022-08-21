489520587fa1a7055d8e2fdaa51b6d2f8dfd5f66
martin <martin@mail.idcduty.com>
2022-08-21 11:38:05 +0400

    办公室IP加白

---
+++ src/main/java/com/gameplat/admin/config/SysConfig.java
@@ -0,0 +1,15 @@
+package com.gameplat.admin.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "system")
+public class SysConfig {
+
+  private List<String> officeIpList;
+}
